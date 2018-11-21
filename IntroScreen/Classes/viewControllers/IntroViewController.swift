c4d0ddc5c1ce1a461b9973e127de6dc1c85495e2
Norman Laudien <norman.laudien1996@gmail.com>
2018-11-21 14:30:34 +0100

    always use the shortest color 'way'

--- IntroScreen/Classes/viewControllers/IntroViewController.swift
+++ IntroScreen/Classes/viewControllers/IntroViewController.swift
@@ -139,8 +139,30 @@ open class IntroViewController: UIPageViewController, UIPageViewControllerDelega
     // MARK: - Color Transition
     
     func changeColor(from startHue: CGFloat, to targetHue: CGFloat, scrolled: CGFloat) {
-        let hueDiff = targetHue - startHue
-        let hue = (hueDiff * scrolled) + startHue
+        let diff = targetHue - startHue
+        let posDiff = diff < 0 ? diff * -1 : diff
+        var hue: CGFloat
+        if 1 - posDiff > posDiff { // shortest way is not over a color border (0 or 1)
+            if diff > 0 { // start < target
+                hue = diff * scrolled + startHue
+            } else { // start > target
+                hue = ((diff * scrolled * -1) - startHue) * -1
+            }
+        } else { // shortest way is over a color border (0 or 1)
+            if diff > 0 { // start < target
+                let total = startHue + 1 - targetHue
+                hue = startHue - (total * scrolled)
+                while hue < 0 {
+                    hue += 1
+                }
+            } else { // start > target
+                let total = 1 - startHue + targetHue
+                hue = startHue + (total * scrolled)
+                while hue > 1 {
+                    hue -= 1
+                }
+            }
+        }
         self.view.backgroundColor = UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
     }
     
