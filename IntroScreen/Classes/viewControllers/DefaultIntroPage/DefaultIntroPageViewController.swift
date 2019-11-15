5b206fd081ed18fc81704041453f086ca00e7a0c
Norman Laudien <kontakt@norman-laudien.de>
2019-11-15 16:13:38 +0100

    master: add text color param to default screen

--- IntroScreen/Classes/viewControllers/DefaultIntroPage/DefaultIntroPageViewController.swift
+++ IntroScreen/Classes/viewControllers/DefaultIntroPage/DefaultIntroPageViewController.swift
@@ -11,23 +11,34 @@ import UIKit
 public class DefaultIntroPageViewController: IntroPageViewController {
     private var subtitle: String?
     private var image: UIImage?
+    private var textColor: UIColor?
     
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var subtitleLabel: UILabel!
     
-    public static func getInstance(index: Int, hue: CGFloat, title: String?, subtitle: String?, image: UIImage?) -> DefaultIntroPageViewController {
+    public static func getInstance(
+        index: Int,
+        hue: CGFloat,
+        title: String?,
+        subtitle: String?,
+        image: UIImage?,
+        textColor: UIColor = .black
+    ) -> DefaultIntroPageViewController {
         let instance = UIStoryboard(name: "DefaultIntroPage", bundle: Bundle(for: self)).instantiateInitialViewController() as! DefaultIntroPageViewController
         instance.index = index
         instance.hue = hue
         instance.title = title
         instance.subtitle = subtitle
         instance.image = image
+        instance.textColor = textColor
         return instance
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        titleLabel.textColor = textColor
+        subtitleLabel.textColor = textColor
         titleLabel.text = title
         subtitleLabel.text = subtitle
         imageView.image = image
